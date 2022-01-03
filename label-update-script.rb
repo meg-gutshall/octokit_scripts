8f09cdaad5d68a4a33f0c295b605871ab02f2232
Meg Gutshall <meghan.gutshall@gmail.com>
2022-01-03 18:35:02 -0500

    Copy script from transfer to label update file

--- label-update-script.rb
+++ label-update-script.rb
@@ -18,119 +18,37 @@ ENV['OCTO_TOKEN']
 # Connect to GitHub account
 client = Octokit::Client.new(access_token: ENV['OCTO_TOKEN'], auto_paginate: true)
 
-# List repos to apply labels to below
-repos = [
-  "meg-gutshall/rxeactions"
-]
+# Load all the repos
+repos = client.repos(client.user.login)
 
-# Create default labels
-default_labels = [
-  {
-    "name": "accessibility :wheelchair:",
-    "description": "Follow a11y best practices",
-    "color": "C93377"
-  },
-  {
-    "name": "dependencies :deciduous_tree:",
-    "description": "Pull requests that update a dependency file",
-    "color": "EE1C75"
-  },
-  {
-    "name": "good first issue :100:",
-    "description": "Good for newcomers",
-    "color": "2074C4"
-  },
-  {
-    "name": "help wanted :sos:",
-    "description": "Up for grabs",
-    "color": "D93F0B"
-  },
-  {
-    "name": "high priority :rotating_light:",
-    "description": "Highest priority issues",
-    "color": "B60205"
-  },
-  {
-    "name": "status: blocked :no_entry_sign:",
-    "description": "Something else is blocking this",
-    "color": "782975"
-  },
-  {
-    "name": "status: on hold :hand:",
-    "description": "Waiting on a decision or review",
-    "color": "375071"
-  },
-  {
-    "name": "status: pending :clock1030:",
-    "description": "In a waiting state",
-    "color": "0D6E0E"
-  },
-  {
-    "name": "type: architecture :classical_building:",
-    "description": "Changes to code architecture",
-    "color": "7057FF"
-  },
-  {
-    "name": "type: bug :bug:",
-    "description": "Something isn't working",
-    "color": "E21D20"
-  },
-  {
-    "name": "type: chore :broom:",
-    "description": "Misc.",
-    "color": "1D7CBB"
-  },
-  {
-    "name": "type: content :speech_balloon:",
-    "description": "Concerning text and literals",
-    "color": "C83462"
-  },
-  {
-    "name": "type: documentation :bookmark_tabs:",
-    "description": "Improvements or additions to documentation",
-    "color": "5A44A0"
-  },
-  {
-    "name": "type: enhancement :sparkles:",
-    "description": "New feature or request",
-    "color": "026A74"
-  },
-  {
-    "name": "type: maintenance :wrench:",
-    "description": "Fixes to and refactoring codebase",
-    "color": "7D2B61"
-  },
-  {
-    "name": "type: needs discussion :speaking_head:",
-    "description": "Further information is needed",
-    "color": "0052CC"
-  },
-  {
-    "name": "type: security :lock:",
-    "description": "Implement a security patch",
-    "color": "E21D20"
-  },
-  {
-    "name": "type: testing :white_check_mark:",
-    "description": "Add test specs",
-    "color": "DB5942"
-  },
-  {
-    "name": "type: ui/ux :lipstick:",
-    "description": "Site styling and appearances",
-    "color": "1A8481"
-  }
-]
+# Select repo(s) you're going to change
+# ! Be sure to downcase the search parameter
+repos.select! do |repo|
+  ## Options include:
+  # repo.fork?
+  # repo.private?
+  repo.name.downcase.include?('tester')
+end
 
-# Remove labels
-remove_labels = {
-  "bug",
-  "documentation",
-  "duplicate",
-  "enhancement",
-  "good first issue",
-  "help wanted",
-  "invalid",
-  "question",
-  "wontfix"
-}
+# Tell us what repos we're about to move
+puts "\nYou've selected the following repos to be transferred: \n".colorize(:light_magenta)
+
+repos.collect do |repo|
+  puts repo.full_name.colorize(:bright_white) + "  >>  ".colorize(:light_yellow) + "Last updated on ".colorize(:light_magenta) + repo.updated_at.strftime("%m/%d/%Y").colorize(:light_yellow)
+end
+
+puts "\nTotal number of search results: ".colorize(:light_magenta) + repos.count.to_s.colorize(:light_yellow)
+
+# This stopped me accidentally breaking everything, remove the "return" line to actually perform the transfers.
+return
+
+
+# Start transferring the repos to the new account
+repos.each do |repo|
+  client.transfer_repo(repo.full_name, STUDY_KIT_ORG, {
+    accept: Octokit::Preview::PREVIEW_TYPES[:transfer_repository]
+  })
+end
+
+# Print success message
+puts "\n#{repos.count.to_s} repos were transferred!".colorize(:light_green)
